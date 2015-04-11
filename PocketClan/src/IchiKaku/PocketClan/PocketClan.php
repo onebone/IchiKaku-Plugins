2b7a094f2ae0626f7d88ba78b5aea43d3c4bdc01
일가크 <woni8708@naver.com>
2015-04-11 19:12:40 +0900

    add onChat and etc.

--- PocketClan/src/IchiKaku/PocketClan/PocketClan.php
+++ PocketClan/src/IchiKaku/PocketClan/PocketClan.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace IchiKaku\criminal;
+namespace IchiKaku\PocketClan;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\command\CommandSender;
@@ -12,12 +13,16 @@ use onebone\economyapi\EconomyAPI;
 
 
 class PocketClan extends PluginBase implements Listener {
-    private $api, $messages;
+    /** @var EconomyAPI */
+    private $api = null;
 
-    public $clanlist, $clandata;
-    public $economy;
+    private $messages;
+    public $m_version = 1;
+    public $clanlist, $clandata, $playerclan;
+    public $clan_list, $clan_data, $player_clan;
 
     public function onEnable() {
+        @mkdir($this->getDataFolder());
         $this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
         if ($this->getServer ()->getPluginManager ()->getPlugin ( "EconomyAPI" ) != null)
             $this->api = EconomyAPI::getInstance ();
@@ -31,7 +36,14 @@ class PocketClan extends PluginBase implements Listener {
         $this->loadData();
         $this->getLogger()->info(TextFormat::GOLD.$this->get("plugin-loaded"));
     }
-
+    public function onDisable() {
+        $this->saveData();
+    }
+    public function onChat(PlayerChatEvent $e) {
+        $e->setCancelled(true);
+        $e->getPlayer()->setRemoveFormat ( false );
+        $this->getServer ()->broadcastMessage ("[".$this->getClan($e->getPlayer()->getName())."]" . $e->getPlayer()->getName() . " : " . $e->getMessage () );
+    }
     /**
      * @param CommandSender $sp
      * @param Command $command
@@ -52,6 +64,7 @@ class PocketClan extends PluginBase implements Listener {
                             $this->clandata[$args[2]][$p] = "admin";
                             $this->clandata[$args[2]]["list"] = array();
                             array_push($this->clandata[$args[2]]["list"] = array(), $p);
+                            $this->playerclan[$p] = $args[2];
                             $sp->sendMessage($this->get("PocketClan-ClanMade") . " [" . $args[2] . "]");
                         }
                         return true;
@@ -60,15 +73,21 @@ class PocketClan extends PluginBase implements Listener {
                             if ($cl == $args[2]) {
                                 $this->clandata[$args[2]][$p] = "user";
                                 array_push($this->clandata[$args[2]]["list"] = array(), $p);
+                                $this->playerclan[$p] = $args[2];
                                 $sp->sendMessage($this->get("PocketClan-ClanJoin") . "\"" . $args[2] . "\"");
                             }
                         }
                         return true;
                     case "list" :
-                        if(isset($args[2]))
-                            $p->sendMessage("[PocketClan]".$args[2]." people : ".sizeof($this->clandata[$args[2]]["list"])."list : ".$this->clandata[$args[2]]["list"]);
-                        else
-                            $p->sendMessage("[PocketClan]".$this->clanlist);
+                        if(isset($args[2])) {
+                            $list = "";
+                            foreach($this->clandata[$args[2]]["list"] as $cl) $list .= $cl.",";
+                            $sp->sendMessage("[PocketClan]" . $args[2] . " people : " . sizeof($this->clandata[$args[2]]["list"]) . "list : " . $list);
+                        } else {
+                            $list = "";
+                            foreach($this->clanlist as $cl) $list .= $cl.",";
+                            $sp->sendMessage("[PocketClan]" . $list);
+                        }
                         return true;
                 }
                 break;
@@ -87,19 +106,37 @@ class PocketClan extends PluginBase implements Listener {
         }
         return true;
     }
+    public function getClan($player) {
+        return $this->playerclan[$player];
+    }
     public function get($var) {
         return $this->messages [$this->messages ["default-language"] . "-" . $var];
     }
     public function loadData() {
-        $this->clanlist = $this->initializeYML ( "clan_list.yml", [ ] );
-        $this->clandata = $this->clanlist->getAll ();
+        $this->clan_list = $this->initializeYML ( "clan_list.yml", [ ] );
+        $this->clan_data = $this->initializeYML("clan_data.yml", []);
+        $this->player_clan = $this->initializeYML("player_clan.yml", []);
+        $this->clandata = $this->clan_data->getAll();
+        $this->clanlist = $this->clan_list->getAll ();
+        $this->playerclan = $this->player_clan->getAll();
     }
     public function saveData() {
-        $this->clanlist->setAll ( $this->clandata);
-        $this->clanlist->save ();
-        $this->defaultTextData ();
+        $this->clan_list->setAll ( $this->clanlist);
+        $this->clan_data->setAll($this->clandata);
+        $this->player_clan->setAll($this->playerclan);
+        $this->clan_list->save ();
+        $this->clan_data->save();
+        $this->player_clan->save();
     }
     public function initializeYML($path, $array) {
         return new Config ( $this->getDataFolder () . $path, Config::YAML, $array );
+    }
+    public function messagesUpdate($targetYmlName) {
+        $targetYml = (new Config ( $this->getDataFolder () . $targetYmlName, Config::YAML ))->getAll ();
+        if (! isset ( $targetYml ["m_version"] )) {
+            $this->saveResource ( $targetYmlName, true );
+        } else if ($targetYml ["m_version"] < $this->m_version) {
+            $this->saveResource ( $targetYmlName, true );
+        }
     }
 }
