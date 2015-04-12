b01a12e80e4908208f3bd3b4d14b847db7464cc9
일가크 <woni8708@naver.com>
2015-04-12 23:13:11 +0900

    modified Error

--- PocketClan/src/IchiKaku/PocketClan/PocketClan.php
+++ PocketClan/src/IchiKaku/PocketClan/PocketClan.php
@@ -15,10 +15,7 @@ use onebone\economyapi\EconomyAPI;
 class PocketClan extends PluginBase implements Listener {
     /** @var EconomyAPI */
     private $api = null;
-
-    private $messages;
-    public $m_version = 1;
-    public $clanlist, $clandata, $playerclan;
+    public $clanlist = [], $clandata = [], $playerclan;
     public $clan_list, $clan_data, $player_clan;
 
     public function onEnable() {
@@ -28,11 +25,8 @@ class PocketClan extends PluginBase implements Listener {
             $this->api = EconomyAPI::getInstance ();
         else {
             $this->getLogger()->error("'EconomyAPI' 플러그인이 없습니다.");
-            $this->getServer ()->getPluginManager ()->disablePlugin ( $this );
+            $this->getServer()->getPluginManager()->disablePlugin($this);
         }
-        $this->saveResource ( "messages.yml", false );
-        $this->messagesUpdate ( "messages.yml" );
-        $this->messages = (new Config ( $this->getDataFolder () . "messages.yml", Config::YAML ))->getAll ();
         $this->loadData();
         $this->getLogger()->info(TextFormat::GOLD."[PocketClan]Plugin was activitied");
     }
@@ -59,38 +53,39 @@ class PocketClan extends PluginBase implements Listener {
                     case "make":
                         if(!isset($args[1])) {
                             $sp->sendMessage("[PocketClan] Please Input ClanName");
-                            return false;
+                            break;
                         }
                         if($this->api->myMoney($p) < 30000) $sp->sendMessage("[PocketClan] You don't have enough money");
-                        else {
+                        else if(isset($args[1])) {
                             $this->api->reduceMoney($p, 30000);
-                            $this->clanlist[$args[2]] = $args[2];
-                            $this->clandata[$args[2]][$p] = "admin";
-                            $this->clandata[$args[2]]["list"] = array();
-                            array_push($this->clandata[$args[2]]["list"], $p);
-                            $this->playerclan[$p] = $args[2];
-                            $sp->sendMessage("[PocketClan] Clan was made " . " [" . $args[2] . "]");
+                            $this->clanlist[$args[1]] = $args[1];
+                            $this->clandata[$args[1]][$p] = "admin";
+                            $this->clandata[$args[1]]["list"] = array();
+                            array_push($this->clandata[$args[1]]["list"], $p);
+                            $this->playerclan[$p] = $args[1];
+                            $sp->sendMessage("[PocketClan] Clan was made " . " [" . $args[1] . "]");
                         }
                         return true;
                     case "join" :
                         if(!isset($args[1])) {
                             $sp->sendMessage("[PocketClan] Please Input ClanName");
-                            return false;
+                            break;
                         }
                         foreach($this->clanlist as $cl) {
-                            if ($cl == $args[2]) {
-                                $this->clandata[$args[2]][$p] = "user";
-                                array_push($this->clandata[$args[2]]["list"], $p);
-                                $this->playerclan[$p] = $args[2];
-                                $sp->sendMessage("[PocketClan] Succesfully joined in  " . "\"" . $args[2] . "\"");
+                            if ($cl == $args[1]) {
+                                $this->clandata[$args[1]][$p] = "user";
+                                array_push($this->clandata[$args[1]]["list"], $p);
+                                $this->playerclan[$p] = $args[1];
+                                $sp->sendMessage("[PocketClan] Succesfully joined in  " . "\"" . $args[1] . "\"");
+                                break;
                             } else $sp->sendMessage("[PocketClan] Can't find Clan");
                         }
                         return true;
                     case "list" :
-                        if(isset($args[2])) {
+                        if(isset($args[1])) {
                             $list = "";
-                            foreach($this->clandata[$args[2]]["list"] as $cl) $list .= $cl.",";
-                            $sp->sendMessage("[PocketClan]" . $args[2] . " people : " . sizeof($this->clandata[$args[2]]["list"]) . "list : " . $list);
+                            foreach($this->clandata[$args[1]]["list"] as $cl) $list .= $cl.",";
+                            $sp->sendMessage("[PocketClan]" . $args[1] . " people : " . sizeof($this->clandata[$args[1]]["list"]) . " list : " . $list);
                         } else {
                             $list = "";
                             foreach($this->clanlist as $cl) $list .= $cl.",";
@@ -100,13 +95,14 @@ class PocketClan extends PluginBase implements Listener {
                     case "leave" :
                         if(!isset($args[1])) {
                             $sp->sendMessage("[PocketClan] Please Input ClanName");
-                            return false;
+                            break;
                         }
-                        if(isset($args[2])) {
-                            if($this->getClan($p) == $args[2]) {
-                                $this->clandata[$args[2]][$p]="NotInClan";
+                        if(isset($args[1])) {
+                            if($this->getClan($p) == $args[1]) {
+                                $this->clandata[$args[1]][$p]="NotInClan";
                                 $this->playerclan[$p] = "none";
-                                array_replace($this->clandata[$args[2]]["list"], $p);
+                                unset($this->clandata[$args[1]]["list"][array_search($p,$this->clandata[$args[1]]["list"])]);
+                                $sp->sendMessage("Succesfully leaved Clan [".$args[1]."]");
                             } else {
                                 $sp->sendMessage("[PocketClan] Clan not founded");
                             }
@@ -135,10 +131,6 @@ class PocketClan extends PluginBase implements Listener {
     public function getClan($player) {
         return $this->playerclan[$player];
     }
-    public function get($var) {
-        //method used by hmmm
-        return $this->messages [$this->messages ["default-language"] . "-" . $var];
-    }
     public function loadData() {
         $this->clan_list = $this->initializeYML ( "clan_list.yml", [ ] );
         $this->clan_data = $this->initializeYML("clan_data.yml", []);
@@ -158,14 +150,5 @@ class PocketClan extends PluginBase implements Listener {
     public function initializeYML($path, $array) {
         //method used by hmmm
         return new Config ( $this->getDataFolder () . $path, Config::YAML, $array );
-    }
-    public function messagesUpdate($targetYmlName) {
-        //method used by hmmm
-        $targetYml = (new Config ( $this->getDataFolder () . $targetYmlName, Config::YAML ))->getAll ();
-        if (! isset ( $targetYml ["m_version"] )) {
-            $this->saveResource ( $targetYmlName, true );
-        } else if ($targetYml ["m_version"] < $this->m_version) {
-            $this->saveResource ( $targetYmlName, true );
-        }
     }
 }
